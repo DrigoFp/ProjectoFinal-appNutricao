5e88b28feff5e44634b5155af6226347680ca1f2
DrigoFp <frodrigofp@gmail.com>
2026-03-23 17:31:55 +0000

    feat: implement POST /food-entries with nutritional calculations and fix database foreign keys

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,16 @@
+// Cria o servidor principal (a API) de backend, recebe pedidos do frontend, fala com o Supabase e devolve respostas em JSON
+// Arranca o servidor
+// Configura middlewares
+// Lê o .env
+// Liga rotas
+// Escuta pedidos na porta 3000
+
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import testRoutes from "./routes/testRoutes.js"
+import foodEntriesRoutes from "./routes/foodEntriesRoutes.js"
+
 
 // Carrega as variáveis do ficheiro .env
 dotenv.config();
@@ -16,6 +26,10 @@ app.use(express.json()); // Permite receber dados em formato JSON
 app.get('/', (req, res) => {
   res.send('API Nutrição Ativa! 🌱');
 });
+
+app.use(testRoutes); // api principal o servidor express, este usa conjunto de rotas (testRoutes) dentro da aplicação principal. Liga essas rotas ao servidor
+
+app.use(foodEntriesRoutes); // é a API que é ligada ao servidor para ter a rota da entrada de comida
 
 // Iniciar o servidor
 app.listen(PORT, () => {
