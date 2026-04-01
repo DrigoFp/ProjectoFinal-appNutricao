891cd4ff321a5de2df94494ff1c31ace0545d7e9
DrigoFp <frodrigofp@gmail.com>
2026-04-01 17:58:35 +0100

    feat: adiciona validação de força de password e bloqueio de botão

--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -10,16 +10,61 @@ import { inject } from '@angular/core';
   styleUrl: './register.css',
 })
 export class Register {
-  // 1. Injetamos as ferramentas
+  // Injetamos as ferramentas
   private authService = inject(AuthService);
   private router = inject(Router);
 
- // 2. A função para o formulário
+  // verificar forca pass no registo
+  larguraBarra: string = '0%';
+  corBarra: string = '#e0e0e0';
+  textoForca: string = '';
+  temNumero: boolean = false;
+
+  verificarForca(senha: string) {
+    const senhaLimpa = senha.trim(); // aqui uso o .trim() para não deixar o utilizador meter espaços
+    const tamanho = senhaLimpa.length; // Aqui uso o .length para verificar o tamanho
+    this.temNumero = /\d/.test(senhaLimpa); // // Esta variável será verdadeira (true) se encontrar pelo menos um número
+
+    if (tamanho === 0) {
+      this.larguraBarra = '0%';
+      this.textoForca = '';
+    } else if (tamanho < 6) {
+      this.larguraBarra = '33%';
+      this.corBarra = '#ff4d4d'; // Vermelho 🔴
+      this.textoForca = 'Fraca';
+    } else if (tamanho >= 8 && this.temNumero) {
+      // Só é forte se tiver 8+ caracteres E um número
+      this.larguraBarra = '100%';
+      this.corBarra = '#2ecc71'; // Verde 🟢
+      this.textoForca = 'Forte';
+    } else {
+      this.larguraBarra = '66%';
+      this.corBarra = '#ffcc00'; // Amarelo 🟡
+      this.textoForca = 'Média';
+    }
+  }
+
+  // A função para o formulário
   carregando = false;
 
   async aoSubmeterRegisto(email: string, pass: string) {
-    // 1. Assim que entra na função, dizemos que começou a carregar
+    // Assim que entra na função, dizemos que começou a carregar
     this.carregando = true;
+    // Limpar e medir a pass
+    const senhaLimpa = pass.trim();
+
+    if (senhaLimpa.length === 0) {
+      alert('A password não pode ser apenas espaços!');
+      this.carregando = false;
+      return;
+    }
+    const temNumero = /\d/.test(senhaLimpa);
+
+    if (!temNumero) {
+      alert('A password deve conter pelo menos um número!');
+      this.carregando = false;
+      return;
+    }
 
     try {
       await this.authService.registar(email, pass);
@@ -28,7 +73,7 @@ export class Register {
       console.error('Erro:', error);
       alert('Dados incorretos!');
     } finally {
-      // 2. O 'finally' corre sempre, quer dê certo ou erro.
+      // O 'finally' corre sempre, quer dê certo ou erro.
       // Aqui dizemos que já terminou de processar.
       this.carregando = false;
     }
